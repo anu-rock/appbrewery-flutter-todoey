579b95cb6a74cac2609cea56a28040eea38f5de3
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-17 18:24:14 +0530

    Wrap TaskList's ListView builder in Consumer
    The Consumer widget comes from Provider package.
    It makes it easy to retrieve the provided state. So no more Provider.of.

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoey/models/task.dart';
 import 'package:todoey/models/task_data.dart';
 import 'task_tile.dart';
 
 class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TaskData taskData = Provider.of<TaskData>(context);
-    List<Task> tasks = taskData.taskList;
-
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          title: tasks[index].name,
-          isChecked: tasks[index].isDone,
-          onChanged: (value) {
-            taskData.toggleTaskDoneness(tasks[index]);
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              title: taskData.taskList[index].name,
+              isChecked: taskData.taskList[index].isDone,
+              onChanged: (value) {
+                taskData.toggleTaskDoneness(taskData.taskList[index]);
+              },
+            );
           },
+          itemCount: taskData.taskList.length,
         );
       },
-      itemCount: tasks.length,
     );
   }
 }
